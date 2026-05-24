d64ccd88a258d0a0c65c214c5f21ce644af1685a
Dang Phuong Nam <dangphuongnam102004@gmail.com>
2026-05-24 09:24:40 +0700

    update: logger reward, remove visualization

--- train_phase1.py
+++ train_phase1.py
@@ -12,42 +12,54 @@ def create_env():
         number_of_left_players_agent_controls=11,
         representation='raw',
         rewards='scoring',
-        render=True
+        render=False,
+        # write_goal_dumps=True,
+        # write_full_episode_dumps=True,
+        # write_video=True,
+        # logdir='experiments/videos',
+        # dump_frequency=5,
     )
 
 
 
 def main():
     base_env = create_env()
-    env = GFootballGlobalWrapper(base_env, num_agents=11)
-    
+    env = GFootballGlobalWrapper(base_env, num_agents=11)  
     agent = HES_COMA_Agent(state_dim=env.state_dim, obs_dim=env.obs_dim, n_actions=10, n_agents=11)
     buffer = RolloutBuffer()
-    logger = CSVLogger('experiments/phase1_training.csv', ['Episode', 'Total_Reward'])
+    logger = CSVLogger('experiments/phase1_training_test.csv', ['Episode', 'Total_reward_buffer',  'Reward_env', 'Reward_energy', 'Reward_ball_owned'])
     
-    n_episodes = 10
-    max_steps = 100
+    n_episodes = 1000
+    max_steps = 150
     
     print("Bắt đầu huấn luyện Phase 1 (Global Agent)...")
     for episode in range(1, n_episodes + 1):
         state, obses = env.reset()
         buffer.clear()
         total_reward = 0
+        view_rewards_env = 0
+        view_rewards_energy = 0
+        view_rewards_ball_owned = 0
         
         for t in range(max_steps):
             actions, _ = agent.get_actions(obses)
 
-            next_state, next_obses, rewards, done = env.step(actions)
+            next_state, next_obses, rewards, view_rewards, done = env.step(actions)
             
             buffer.store(state, obses, actions, rewards, next_state, done)
+
             total_reward += np.sum(rewards)
-            
+            view_rewards_env += np.sum(view_rewards['rewards_env'])
+            view_rewards_energy += np.sum(view_rewards['rewards_energy'])  
+            view_rewards_ball_owned += np.sum(view_rewards['rewards_ball_owned'])
+
             state, obses = next_state, next_obses
             if done: break
                 
         agent.update(buffer)
-        logger.log([episode, total_reward])
-        print(f"Episode: {episode}/{n_episodes} | Lợi nhuận: {total_reward:.2f}")
+        logger.log([episode, total_reward, view_rewards_env, view_rewards_energy, view_rewards_ball_owned])
+
+        print(f"Episode: {episode}/{n_episodes} | Total_reward: {total_reward:.2f} | Reward_env: {view_rewards_env:.2f} | Reward_energy: {view_rewards_energy:.2f} | Reward_ball_owned: {view_rewards_ball_owned:.2f}")
 
     # Thay thế đoạn torch.save cũ bằng:
     os.makedirs('experiments/models', exist_ok=True)
